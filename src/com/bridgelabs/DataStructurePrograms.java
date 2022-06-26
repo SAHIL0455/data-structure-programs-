dabc8da321df1264a10b8ad41011421957efabf2
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-27 01:47:00 +0530

    Initial

---
+++ src/com/bridgelabs/DataStructurePrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabs;
+
+public class DataStructurePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure Programs");
+    }
+}
